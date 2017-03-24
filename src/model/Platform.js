5c29153da9565b4800274c503fedd887bb4c432c
neu-wangye <wangye1@husky.neu.edu>
2017-03-24 19:07:56 -0400

    change platform, user and reviews

--- src/model/Platform.js
+++ src/model/Platform.js
@@ -2,35 +2,21 @@
  * Created by WangYe on 3/20/17.
  */
 const Sequelize = require("sequelize");
-const config  = require ("../config.js");
 
-var sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 30000
-    }
-});
+export const defp = (sequelize)=>{
 
-
-
-let Platform = sequelize.define('platform', {
-    gameId: {
-        type: Sequelize.BIGINT(50),
-        primaryKey: true
-    },
-    platformName: {
-        type: Sequelize.STRING(50),
-        primaryKey: true
-    },
-    createdAt: Sequelize.BIGINT,
-    updatedAt: Sequelize.BIGINT
-}, {
-    timestamps: false
-});
-
-//Platform.sync();
-
-module.exports = Platform;
+    return sequelize.define('platform', {
+        gameId: {
+            type: Sequelize.BIGINT(50),
+            primaryKey: true
+        },
+        platformName: {
+            type: Sequelize.STRING(50),
+            primaryKey: true
+        },
+        createdAt: Sequelize.BIGINT,
+        updatedAt: Sequelize.BIGINT
+    }, {
+        timestamps: false
+    });
+};
--- src/model/Review.js
+++ src/model/Review.js
@@ -2,34 +2,22 @@
  * Created by WangYe on 3/20/17.
  */
 const Sequelize = require("sequelize");
-const config  = require ("../config.js");
 
-let sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 30000
-    }
-});
+export const defr = (sequelize)=>{
 
-let Review = sequelize.define('review', {
-    id: {
-        type: Sequelize.BIGINT(50),
-        autoIncrement: true,
-        primaryKey: true
-    },
-    userId: Sequelize.BIGINT(100),
-    gameId: Sequelize.BIGINT(100),
-    rate: Sequelize.BIGINT,
-    content: Sequelize.STRING(100),
-    createdAt: Sequelize.BIGINT,
-    updatedAt: Sequelize.BIGINT
-}, {
-    timestamps: false
-});
-
-//Review.sync();
-
-module.exports = Review;
+    return sequelize.define('review', {
+        id: {
+            type: Sequelize.BIGINT(50),
+            autoIncrement: true,
+            primaryKey: true
+        },
+        userId: Sequelize.BIGINT(100),
+        gameId: Sequelize.BIGINT(100),
+        rate: Sequelize.BIGINT,
+        content: Sequelize.STRING(100),
+        createdAt: Sequelize.BIGINT,
+        updatedAt: Sequelize.BIGINT
+    }, {
+        timestamps: false
+    });
+};
