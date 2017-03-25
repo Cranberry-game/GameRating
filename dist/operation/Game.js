81262a53f6d5ac671d8c3adc815c71f89dd075af
neu-wangye <wangye1@husky.neu.edu>
2017-03-24 20:09:55 -0400

    new operation to game

--- dist/operation/Game.js
+++ dist/operation/Game.js
@@ -66,3 +66,43 @@ var queryGameById = exports.queryGameById = function queryGameById(id, Game) {
         }
     })();
 };
+
+var queryGameByName = exports.queryGameByName = function queryGameByName(name, Game) {
+    //search a game
+    (async function () {
+        var game = await Game.findAll({
+            limit: 10,
+            where: {
+                title: {
+                    $like: '%' + name + '%'
+                }
+            },
+            order: [['totalRate', 'DESC']]
+        });
+        console.log('find ' + game.length + ' game:');
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
+
+        try {
+            for (var _iterator2 = game[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                var p = _step2.value;
+
+                console.log(JSON.stringify(p));
+            }
+        } catch (err) {
+            _didIteratorError2 = true;
+            _iteratorError2 = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                    _iterator2.return();
+                }
+            } finally {
+                if (_didIteratorError2) {
+                    throw _iteratorError2;
+                }
+            }
+        }
+    })();
+};
