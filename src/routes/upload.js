042a79c866c3e89ea097ed7afff71dcbe3e4766d
dengzb <vogons@live.com>
2017-04-03 20:41:06 -0400

    multi files upload in scshot

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -7,20 +7,21 @@ let formidable = require('formidable');
 let path = require('path');
 
 router.route('/avatar')
-      .post(jsonParser,async (req,res,next)=>{
-      let form = new formidable.IncomingForm();
-         form.uploadDir = await router.get('dir')+"/public/avatar";
-         form.parse(req,(err,fields,files)=>{
+      .post(jsonParser,async(req,res,next)=>{
+          let form = new formidable.IncomingForm();
+          form.keepExtensions=true;
+          form.uploadDir = router.get('dir')+"/public/avatar";
+          form.parse(req,(err,fields,files)=>{
            let filename= path.basename(files.files.path);
-            res.send(JSON.stringify({url:'gamerating.info/avatar/'+filename}));
+           res.send(JSON.stringify({url:'gamerating.info/avatar/'+filename}));
            console.log('upload success');
-           next();
-          
-        });
+          next();
+          });
       });
 router.route('/cover')
   .post(jsonParser,async (req,res,next)=>{
     let form = new formidable.IncomingForm();
+        form.keepExtensions=true;
          form.uploadDir = await router.get('dir')+"/public/cover";
          form.parse(req,(err,fields,files)=>{
            let filename= path.basename(files.files.path);
@@ -36,10 +37,17 @@ router.route('/cover')
 router.route('/scshot')
   .post(jsonParser,async (req,res,next)=>{
     let form = new formidable.IncomingForm();
+        form.keepExtensions=true;
+        form.multiples = true;
          form.uploadDir = router.get('dir')+"/public/screenshot";
-         form.parse(req,(err,fields,files)=>{
-           let filename= path.basename(files.files.path);
-           res.send(JSON.stringify({url:'gamerating.info/screenshot/'+filename}));
+         form.parse(req,async (err,fields,files)=>{
+            let urls = new Array(files.files.length);
+            for(let i=0;i<files.files.length;i++){
+              let filename = await path.basename(files.files[i].path);
+              urls[i] = 'gamerating.info/screenshot/'+filename; 
+            }
+            
+            res.send(JSON.stringify({url:urls}));
            console.log('upload success');
               next();
         });
@@ -48,6 +56,5 @@ router.route('/scshot')
 
 
   });
-
 
 
